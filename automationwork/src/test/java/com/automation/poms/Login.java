d1d6545a681811883ad17db44c075680bbe359f3
Yonka556 <amanda975@revature.net>
2022-07-14 22:57:27 -0400

    Acceptance

--- automationwork/src/test/java/com/automation/poms/Login.java
+++ automationwork/src/test/java/com/automation/poms/Login.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.automation.poms;
 
 import org.openqa.selenium.WebDriver;
@@ -69,75 +69,4 @@ public class Login {
             this.loginButton.click();
     }
 }
-=======
-package com.automation.poms;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
-// this class is going to be my java version of my login webpage
-public class Login {
-    /*
-      By defining what web elements you want you work with here instead of in the steps themselve we avoid
-      writing "boilerplate" (think redundant/repetitve) code.
-     */
-
-    private WebDriver driver; //this driver will only be used internally so its class is private
-
-    // these WebElements need to referenced in our steps so they are public
-
-    public WebDriver getDriver() {
-        return driver;
-    }
-
-    public void setDriver(WebDriver driver) {
-        this.driver = driver;
-    }
-    //this tells the PageFactory to find the element with the id username and assign it to this field
-    @FindBy(id = "username") 
-    public WebElement usernameInput;
-
-    // this tells the PageFactory to find the element with the id password and assign it to this field
-    @FindBy(id = "password")
-    public WebElement passwordInput;
-
-    //this tell the PageFactory to find the first element created via button tags and assign it to this field
-    @FindBy(tagName = "button")
-    public WebElement loginButton;
-
-    public Login(WebDriver driver){
-        this.driver = driver; //this initalizes the private driver field
-
-        /*
-         The PageFactory class is provided by Selenium and it abstracts away from us the code that handles 
-         initializing our WebElement fields. We simply provide the initElement() method with the private driver
-         We initialize above, and the PageFactory handles he rest.
-         */
-        PageFactory.initElements(driver,this);
-  
-    }
-
-    /*
-      these methods are public so we can access them in the steps and the return type is void because the functions do not return
-      any value they just interact with the webpage
-     */
-
-     //this method will allow both manager and tester to enter their usernames
-    public void enterUsername(String username){
-        //to enter a username into the input element we use the sendKeys method
-        this.usernameInput.sendKeys(username);
-    }
-
-    //this method will allow both manager and tester to enter their passwords
-    public void enterPassword(String password){
-        this.passwordInput.sendKeys(password);
-    }
-    // this method will make selenium click on our login button
-    public void clickButton(){
-            //to click on an element we just use the click() method
-            this.loginButton.click();
-    }
-}
->>>>>>> 3d0b98e7029399d3af9fa85c3f57caccd89fad20
--- automationwork/src/test/java/com/automation/steps/TesterSteps.java
+++ automationwork/src/test/java/com/automation/steps/TesterSteps.java
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
- package com.automation.steps;
+package com.automation.steps;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
@@ -61,67 +60,3 @@ public class TesterSteps {
 
     
 
-=======
- package com.automation.steps;
-
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.Select;
-
-import com.automation.runner.TestRunner;
-
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-
-public class TesterSteps {
-    
-
-    @Given("the tester is on their correct home page")
-    public void the_tester_is_on_their_correct_home_page(){
-        TestRunner.driver.get("File://C:/Users/Arod1/OneDrive/Desktop/VSC/Maven/Foundations-Project1/automationwork/src/test/resource/web-pages/tester-page.html");
-    }
-
-    @When("the tester accepts the defect")
-    public void the_tester_accepts_the_defect(){
-        Select testdefect = new Select(TestRunner.driver.findElement(By.name("Accepted")));
-        testdefect.selectByVisibleText("accepted");}
-    
-    
-
-    @When("the tester declines the defect")
-    public void the_tester_declines_the_defect(){
-        Select testdefect = new Select(TestRunner.driver.findElement(By.name("Accepted")));
-        testdefect.selectByVisibleText("declined");}
-
-    
-
-    @When("the tester fix the defect")
-    public void the_tester_fix_the_defect(){
-        Select testdefect = new Select(TestRunner.driver.findElement(By.name("Fix")));
-        testdefect.selectByVisibleText("Fix");
-    }
-    @When("the tester shelves the defect")
-    public void the_tester_shelve_the_defect(){
-        Select testdefect = new Select(TestRunner.driver.findElement(By.name("Fix")));
-        testdefect.selectByVisibleText("Shelved");
-    }
-
-
-    @When("the tester clicks the submit button")
-    public void the_tester_clicks_the_submit_button(){
-        TestRunner.tester.clickButton();
-    }
-
-    @Then("the tester returns to the login page")
-    public void the_tester_returns_to_the_login_page(){
-        TestRunner.driver.get("File://C:/Users/Arod1/OneDrive/Desktop/VSC/Maven/Foundations-Project1/automationwork/src/test/resource/web-pages/P1html-login.html");
-        String title = TestRunner.driver.getTitle();
-        Assert.assertEquals("Welcome to Dirty Inc. Login", title);
-    }
-
-}
-
-    
-
->>>>>>> 3d0b98e7029399d3af9fa85c3f57caccd89fad20
